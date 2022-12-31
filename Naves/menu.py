d1be971bd7097b762945f4655b7c45c05f98b6eb
Adriana Pava <adrianapava92@gmail.com>
2022-12-30 20:32:33 -0500

    Refactorizacion de codigo y creacion de metodo buscar por tipo y ID

--- Naves/menu.py
+++ Naves/menu.py
@@ -1,38 +1,51 @@
 from pprint import pprint
 from PyInquirer import prompt, Separator
 from preguntas.preguntasBase import questionsLayer1, questionsSeleccionarTipo
-from preguntas.preguntasBase import crearTipo1, crearTipo2, crearTipo3
+from preguntas.preguntasBase import crearTipo1, crearTipo2, crearTipo3, layerBuscar,crearBusquedaPorId
 from tiposNave.nave_tipo1 import nave1
 from tiposNave.nave_tipo2 import nave2
 from tiposNave.nave_tipo3 import nave3
-
+from database import database
+from helpers.inventarioNaves import guardarInventario
 
 
 def  nave_tipo1():
     values = prompt(crearTipo1)
-    print(values)
-    resultado = nave1(values["nombre_nave"], values["Pais_creacion"], values["Peso_nave"], values["n_id"], values["Capacidad_transporte"], values["Capacidad_empuje"])
-    resultado.guardarBaseDatos(values)
-
+    naveCreada = nave1(values)
+    naveCreada.guardar()
 
 
 def  nave_tipo2():
     values = prompt(crearTipo2)
-    print(values)
-    resultado = nave2(values["nombre_nave"], values["Pais_creacion"], values["Peso_nave"], values["n_id"], values["Velocidad_llegada"])
-    resultado.guardarBaseDatos(values)
+    naveCreada = nave2(values)
+    naveCreada.guardar()
+
 
 def  nave_tipo3():
     values = prompt(crearTipo3)
-    print(values)
-    resultado = nave3(values["nombre_nave"], values["Pais_creacion"], values["Peso_nave"], values["n_id"], values["Capacidad_Personas"], values["Capacidad_Orbita"] )
-    resultado.guardarBaseDatos(values)
+    naveCreada = nave3(values)
+    naveCreada.guardar()
+
+
+def buscarId():
+    values = prompt(crearBusquedaPorId)
+    result = database.buscarporId(values)
+    print("####### Estos son los datos de la nave selccionada: ")
+    print(result)
+
+
+def buscarporTipo():
+    values = prompt(questionsSeleccionarTipo)
+    results = database.buscarPorTipo(values)
+    print("####### Estos son los datos de las naves encontrada: ")
+    for r in results:
+        print(r)
+
  
 def seleccionar_tipo():
     print("##### Vamos a crear tu nave, por favor dinos:")
     respuesta = prompt(questionsSeleccionarTipo)
-    tipo_seleccionado = respuesta["tipo"]
-    print(tipo_seleccionado)
+    tipo_seleccionado = respuesta["tipo_nave"]
 
     if tipo_seleccionado == "Lanzadera":
         nave_tipo1()
@@ -43,27 +56,30 @@ def seleccionar_tipo():
     print("Felicitaciones, tu nava ha sido creada exitosamete")
 
 
+def buscar_tipo():
+    print("##### Vamos a buscar la nave")
+    naveAbuscar = prompt(layerBuscar)
+    filtro_seleccionado = naveAbuscar["naveBuscar"]
+
+    if filtro_seleccionado == "ID":
+       buscarId()
+    elif filtro_seleccionado == "Tipo":
+        buscarporTipo()
+    print("Felicitaciones, tu nave ha sido encontrada")
+
+
 
 accion_seleccionada=""
 
-while accion_seleccionada != "salir":
+
+guardarInventario()
+
+while accion_seleccionada != "salir": 
     layer1 = prompt(questionsLayer1)
     accion_seleccionada = layer1["accion"]
 
     if accion_seleccionada == "crear":
         seleccionar_tipo()
     elif accion_seleccionada == "buscar":
-        layerBuscar =  prompt(questionsBuscar)
+        buscar_tipo()
     
-
-
-
-
-
-
-
-
-
-
-
-
