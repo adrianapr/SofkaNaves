920016fdb6b3de56027469d9d4f794cc8272602c
Adriana Pava <adrianapava92@gmail.com>
2022-12-31 03:08:08 -0500

    Variabiliza la URL de la base de datos

--- Naves/database/database.py
+++ Naves/database/database.py
@@ -1,5 +1,15 @@
 from pymongo import MongoClient
-MONGO_URI = 'mongodb://localhost:27017'
+import os
+
+ipBaseDatos= os.environ.get('DB_HOST')
+print(ipBaseDatos)
+
+if ipBaseDatos == None:
+   MONGO_URI = 'mongodb://localhost:27017'
+else:
+    MONGO_URI = 'mongodb://'+ipBaseDatos+':27017'
+
+print(MONGO_URI)
 
 cliente = MongoClient(MONGO_URI)
 
